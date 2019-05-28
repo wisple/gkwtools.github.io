59b21777bb9a5cf9064f4b1096e03e573e3ec675
Guilherme Diego <guidiego.expgames@gmail.com>
2019-05-28 08:44:06 -0300

    Upgrade Site

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    // Responsive Menu
     const nav = document.getElementById('nav');
     const menu = document.getElementById('menu');
 
@@ -6,4 +7,30 @@ window.onload = function () {
         this.classList.toggle('close');
         nav.classList.toggle('open');
     }
+
+    // Features
+    const featureSection = document.getElementById('features');
+    const featureBtns = featureSection.querySelectorAll('.list div');
+    const featureFrames = featureSection.querySelectorAll('.content div');
+
+    featureBtns.forEach((btn) => {
+        btn.onclick = function () {
+            featureBtns.forEach((otherBtns) => {
+                otherBtns.classList.remove('active');
+            });
+
+            this.classList.add('active');
+
+            featureFrames.forEach((frame) => {
+                if (frame.dataset.frame === this.dataset.select) {
+                    frame.classList.add('active')
+                } else {
+                    frame.classList.remove('active')
+                }
+            });
+        }
+    });
+
+    featureBtns[0].classList.add('active');
+    featureFrames[0].classList.add('active');
 }
